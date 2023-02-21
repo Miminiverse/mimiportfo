f3db06252be4f23e57bbbf098756c8b36df7cc8f
mimi <minhtamphamtol@gmail.com>
2023-02-21 00:09:07 -0500

    search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import About from "../components/About";
 import Projects from "../components/Projects";
 import Header from "../components/Header";
 import Contact from "../components/Contact";
+import Search from "../components/Search";
 
 const Home = () => {
   return (
     <>
     <Header />
     <Hero />
+    <Search />
     <Projects />
     <About />
     <Contact />
