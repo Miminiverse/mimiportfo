facee358db151f3e4187440894cef0ff2e91f23c
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-09-15 16:56:04 -0400

    update resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -12,45 +12,23 @@ const Resume = () => {
         <h4>EXPERIENCE</h4>
         <p>
           <br />
-          <h5>Sales Representative & Production Assistant </h5>
-          <h6>Sep 2019 to May 2021 | Leemode International | USA </h6>
+          <h5>Code The Dream | USA </h5>
+          <h6>July 2023 to September 2023 </h6>
           <ul>
             <li>
-              ● Service accounts for more than 50 companies providing revenue of
-              $100,000 per month{" "}
+              ● Receive intensive training in front-end software development
             </li>
-            <li>
-              ● Maintain and develop relationship with the top 5 company biggest
-              customers{" "}
-            </li>
-            <li>
-              ● Answer customer questions about products, prices, availability
-              and credit terms through phone calls/emails{" "}
-            </li>
-            <li>● Solve customer delivery and return issues </li>
-            <li>
-              ● Identify appropriate prospects, meet with customer in showroom
-              and make effective sales{" "}
-            </li>
-            <li>
-              ● Generate and utilize sales reports to achieve sales goals{" "}
-            </li>
-            <li>● Obtain deposits and balance of payment from clients </li>
-            <li>
-              ● Handle stock, inventory, warehouse and assist production team
-              with relevant logistic documentation
-            </li>
+            <li>● Build MentorUp: Class Management System Application</li>
           </ul>
         </p>
         <p>
-          <h5>Business Development Intern</h5>
-          <h6>May 2019 - Sep 2019 | Source of Asia | Vietnam </h6>
+          <h5>Code The Dream | USA</h5>
+          <h6>May 2023 - July 2023 </h6>
           <ul>
             <li>
-              ● Contact associations or prospects to introduce the company{" "}
+              ● Receive intensive training in backend software development
             </li>
-            <li>● Organize business meeting event </li>
-            <li>● Build database of potential partners </li>
+            <li>● Build Talckatoo: Real-Time Multilingual Chat Application</li>
           </ul>
         </p>
         <br />
@@ -60,95 +38,60 @@ const Resume = () => {
           <br />
           <h5>Computer System Institute </h5>
           <h6>NCP Web Development | USA </h6>
-          <p>Jan 2023 - Now</p>
-          <h5>English as a Second Language - ESL</h5>
-          <h6>Campus Education | USA </h6>
-          <p>Jan 2022 - Dec 2022 </p>
-          <h5>Business English Interpretation </h5>
-          <h6>August 2016 - August 2019 | American Polytechnic College </h6>
-          <p>Academic rating: good </p>
-          <p>Year GPA: 8.9/10 </p>
+          <p>January 2023 - December 2023</p>
         </p>
+        <br />
 
-        <h4>COMMUNITY ENGAGEMENT </h4>
+        <h4>VOLUNTEERING</h4>
         <p>
-          <h5>Event Team Leader </h5>
-          <h6>July 2015 - Present | English Speaking Club</h6>
+          <h5>Backend Developer </h5>
+          <h6>August 2023 - Present | Dev Launchers</h6>
           <ul>
-            <li>● Christmas Event 2016 - Leader </li>
-            <li>
-              ● Annual charity event for Down syndrome children 2017 -
-              Coordinator{" "}
-            </li>
-            <li>● Speech Contest 2018 Wings up! - Coordinator </li>
-            <li>● Summer Camp 2018 – Leader </li>
-          </ul>
-        </p>
-        <p>
-          <h5>Volunteer </h5>
-          <ul>
-            <li>
-              ● Volunteer in operating entrepreneur event and book fair event{" "}
-            </li>
-            <li>● Participate in storytelling contest </li>
-            <li>
-              ● Volunteer in Smile'' Restaurant - a chain of restaurants for
-              poor people Achievements{" "}
-            </li>
-            <li>
-              ● Certificate of participation from The U.S Consulate General Ho
-              Chi Minh City in 2018{" "}
-            </li>
-            <li>
-              Certificate of Appreciation from Hatch! Fair 2016 - Invisible
-              Technology
-            </li>
-            <li>
-              Certificate of Appreciation from The U.S Consulate General Ho Chi
-              Minh City in 2016{" "}
-            </li>
+            <li>● Build backend system using Strapi </li>
+            <li>● Write integration testing using Postman & Chai</li>
+            <li>● Review other team member's PR </li>
           </ul>
         </p>
 
-        <p>
-          <h5>Skills </h5>
-          <h6>Sales Skills </h6>
-          <ul>
-            <li>● Sales software and Microsoft office proficiency </li>
-            <li>
-              ● Maintain profitable customer relationship and customer retention{" "}
-            </li>
-            <li>● Use internal and external data to grow sales </li>
-            <li>
-              ● Time management include prioritizing, multitasking and task
-              delegation
-            </li>
-          </ul>
-        </p>
-
-        <p>
-          <h6>Event Management Skills </h6>
-          <ul>
-            <li>● Create attractive and detailed proposals </li>
-            <li>● Prepare a request for estimates and quotations </li>
-            <li>● Develop reports for risk management in the event </li>
-            <li>
-              ● Process post-event summaries, invoices, and critiqued execution
-              of event{" "}
-            </li>
-          </ul>
-        </p>
+        <br />
+        <h4>SKILLS </h4>
+        <h5>Backend Skills </h5>
+        <ul>
+          <li>● NodeJS, Express, Typescript, Django </li>
+          <li>Headless CMS: Strapi</li>
+          <li>
+            Database & Storage: MongoDB, GraphQL/Hygrapgh, PostgreSQL, SQLite,
+            AWS S3{" "}
+          </li>
+          <li>Real-time: Websocket | SocketIO </li>
+          <li>Hosting: Heroku, Digital Ocean, Vercel, Netlify, Render </li>
+          <li>Container: Docker</li>
+          <li>API testing: Postman, Chai</li>
+          <li>Version & Project management: Git | GitHub | Jira</li>
+          <li>Authentication: OAuth, Passport</li>
+        </ul>
+        <h5>Frontend Skills </h5>
+        <ul>
+          <li>● React, Typescript, TailwindCSS </li>
+        </ul>
+        <h5>Designer Documentation</h5>
+        <ul>
+          <li>● Write technical documentation </li>
+        </ul>
+        <br />
 
         <h4>REFERENCES </h4>
         <p>
           <br />
-          <h5>Lisa Kim</h5>
-          <h6>Sep 2019 to May 2021 | Leemode International | USA </h6>
-          <p>Email: lisak@leemode.com </p>
-
-          <h5>Nguyen Thi Phuong Thao </h5>
-          <h6>CEO & Founder | Vietnamese Language Garden </h6>
-          <p>Email: thao.nguyen@vietnameselanguagegarden.vn</p>
+          <h5>Frank Stepanski</h5>
+          <h6> Code The Dream admin/mentor </h6>
+          <p>Email: fsjay@hotmail.com</p>
+          <h5> Elijah Bernstein-Cooper</h5>
+          <h6>
+            Code The Dream mentor | Senior Software Engineer at MetaMetrics,
+            Inc.
+          </h6>
+          <p>e.bernsteincooper@gmail.com</p>
         </p>
       </div>
     </section>
